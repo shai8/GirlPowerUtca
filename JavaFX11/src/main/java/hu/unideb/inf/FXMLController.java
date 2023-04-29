4e792193245e406562f538bc72c1bcb0007c9d41
Chaima <88341102+shai8@users.noreply.github.com>
2023-04-29 18:55:32 +0200

    login commit

--- JavaFX11/src/main/java/hu/unideb/inf/FXMLController.java
+++ JavaFX11/src/main/java/hu/unideb/inf/FXMLController.java
@@ -7,13 +7,32 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
+
+import javafx.scene.control.Button;
+
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class FXMLController implements Initializable {
 
 
 
+
+
+    @FXML
+    private Button button;
+
+    @FXML
+    private TextField id;
+
     @FXML
     private Label label;
 
+    @FXML
+    private PasswordField pass;
+
+    @FXML
+    private Button sign;
     @FXML
     void LogoutButtonAction(ActionEvent event) {
 
