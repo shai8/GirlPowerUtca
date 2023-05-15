7c070b7199a7f246fec5e1fe0d0be2bd10538af3
Chaima <88341102+shai8@users.noreply.github.com>
2023-05-16 00:45:39 +0200

    new home UI

--- JavaFX11/src/main/java/hu/unideb/inf/HOMEController.java
+++ JavaFX11/src/main/java/hu/unideb/inf/HOMEController.java
@@ -1,4 +1,65 @@
 package hu.unideb.inf;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class HOMEController {
+
+    @FXML
+    private Label Borrowlabel;
+
+    @FXML
+    private Label DOBlabel;
+
+    @FXML
+    private Label EmailLabel;
+
+    @FXML
+    private Label PhoneLabel;
+
+    @FXML
+    private Button accountbutton;
+
+    @FXML
+    private Button bookbutton;
+
+    @FXML
+    private Label n;
+
+    @FXML
+    private Label namelabel;
+
+    @FXML
+    private Label overduelabel;
+
+    @FXML
+    private Button savebutton;
+
+    @FXML
+    private Button signoutbutton;
+
+    @FXML
+    private Label surnamelabel;
+
+    @FXML
+    void actionOnSaveButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionOnbookbutton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionOnsignoutButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actiononaccountbutton(ActionEvent event) {
+
+    }
+
 }
