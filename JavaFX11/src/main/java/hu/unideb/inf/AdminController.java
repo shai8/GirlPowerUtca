0490a95de8d164d37e9ba08fbe4c5bbd76cc715b
Chaima <88341102+shai8@users.noreply.github.com>
2023-05-16 11:36:04 +0200

    new home UI

--- JavaFX11/src/main/java/hu/unideb/inf/AdminController.java
+++ JavaFX11/src/main/java/hu/unideb/inf/AdminController.java
@@ -1,4 +1,57 @@
 package hu.unideb.inf;
 
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class AdminController {
+
+    @FXML
+    private TextField ISBNlabel;
+
+    @FXML
+    private TextField authorlabel;
+
+    @FXML
+    private TextArea bookdisplaylabel;
+
+    @FXML
+    private TextField dobLabel;
+
+    @FXML
+    private TextField genrelabel;
+
+    @FXML
+    private Button helpbutton;
+
+    @FXML
+    private TextField langlabel;
+
+    @FXML
+    private Button reminderbutton;
+
+    @FXML
+    private Button signoutbutton;
+
+    @FXML
+    private TextField titlelabel;
+
+    @FXML
+    void actionOnsignoutButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionhelpbutton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionreminderbutton(ActionEvent event) {
+
+    }
+
 }
