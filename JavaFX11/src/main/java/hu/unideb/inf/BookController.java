d65408559cfea7fa843df89329389a78c3bd03f9
Chaima <88341102+shai8@users.noreply.github.com>
2023-05-16 11:06:48 +0200

    new home UI

--- JavaFX11/src/main/java/hu/unideb/inf/BookController.java
+++ JavaFX11/src/main/java/hu/unideb/inf/BookController.java
@@ -1,4 +1,55 @@
 package hu.unideb.inf;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
-public class BookController {
+public class BookController  {
+
+    @FXML
+    private TextField ISBNlabel;
+
+    @FXML
+    private Button accountbutton;
+
+    @FXML
+    private TextField authorlabel;
+
+    @FXML
+    private Button bookbutton;
+
+    @FXML
+    private TextArea bookdisplaylabel;
+
+    @FXML
+    private TextField dobLabel;
+
+    @FXML
+    private TextField genrelabel;
+
+    @FXML
+    private TextField langlabel;
+
+    @FXML
+    private Button signoutbutton;
+
+    @FXML
+    private TextField titlelabel;
+
+    @FXML
+    void actionOnbookbutton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actionOnsignoutButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void actiononaccountbutton(ActionEvent event) {
+
+    }
+
 }
